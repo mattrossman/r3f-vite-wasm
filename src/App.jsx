c7092edfe97b714cc0cd786aa9d641d17c2c875d
mattrossman <rossman@gatech.edu>
2021-06-19 20:25:41 -0400

    Switch from TS to JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 import React, { useEffect } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import init, { greet } from 'wasm-test';
+import init, { greet } from 'wasm-test'
 
 function App() {
   useEffect(() => {
-    init();
+    init()
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello WebAssembly!</p>
         <p>Vite + Rust + React</p>
         <p>
-          <button onClick={() => greet('wasm')}>
-            hello wasm
-          </button>
+          <button onClick={() => greet('wasm')}>hello wasm</button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
       </header>
     </div>
